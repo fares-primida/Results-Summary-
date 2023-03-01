d4b04619dd9cccc4b8ed04a04d46985c7ac7bab5
fares <fares12yaseer45@gmail.com>
2023-03-01 02:11:51 +0200

    finish the left Box and Start Design Right Box

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -3,7 +3,31 @@ import './Style.scss'
 
 const Project = () => {
     return (
-        <div>Project</div>
+        <div className='container'>
+            <div className="result">
+                <div className="header">
+                    <h3>Your Result</h3>
+                </div>
+                <div className="circle">
+                    <div className="CircleBox">
+                        <h3>76</h3> <span>of 100</span>
+                    </div>
+                </div>
+                <div className="title">
+                    <h2>Great</h2>
+                </div>
+                <div className="Description">
+                    <p>Your Score Heigher Than 65% Of <br /> The People Who Have Taken<br /> This Test</p>
+                </div>
+            </div>
+            <div className="results">
+                <div className="Summery">
+                    Summary
+                </div>
+                <div className="EndResults"></div>
+                <div className="btn"></div>
+            </div>
+        </div>
     )
 }
 
